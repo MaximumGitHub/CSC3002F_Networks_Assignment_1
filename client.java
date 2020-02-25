0cae63c52aa171b85f27084cfd5254eac6c21ab8
MaximumGitHub <58106349+MaximumGitHub@users.noreply.github.com>
2020-02-25 15:39:08 +0200

    Testing for the first sending of a file.

--- client.java
+++ client.java
@@ -1,20 +1,77 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.lang.*;
+import java.net.Socket;
 
-import java.io.IOException;
-import java.net.*;
+/**
+ * A command line client for the date server. Requires the IP address of the
+ * server as the sole argument. Exits after printing the response.
+ */
+public class client {
 
-    public class client
+    public static void main(String[] args) throws IOException
     {
 
-        public static void main(String[] args) throws IOException
+        boolean flag = false;
+
+        Scanner sc  = new Scanner(System.in);
+        while(!flag)
         {
-            Foo();
+            System.out.println("Welcome to Jeff. Please enter 1 to upload a file and 2 to download a file. Enter 0 to quit.");
+            int input = sc.nextInt();
+            if(input == 1)
+            {
+
+            }
+            else if(input == 2)
+            {
+
+            }
+            else
+            {
+                flag = false;
+            }
         }
-        static void Foo() throws IOException {
 
-            Socket client = new Socket("196.24.164.225",1024); //Initialize server socket here.
+        var socket = new Socket(InetAddress.getLocalHost(), 59090);
+        var in = new Scanner(socket.getInputStream());
+        System.out.println("Server response: " + in.nextLine());
+        BufferedInputStream bufferedIS = null;
 
-            String hostName = client.getInetAddress().getHostName();
-
-            System.out.println(hostName);
+        //Declaration with try catch for the input and output streams of the socket called socket.
+        DataOutputStream output = null;
+        try
+        {
+            output = new DataOutputStream(socket.getOutputStream());
         }
+        catch (IOException e)
+        {
+            System.out.println(e);
+        }
+        DataInputStream input = null;
+        try
+        {
+            input = new DataInputStream(socket.getInputStream());
+        }
+        catch (IOException e)
+        {
+            System.out.println(e);
+        }
+
+        File file = new File("DISGOSTANG.jpg");
+        int count;
+        byte[] buffer = new byte[(int)file.length()]; // or 4096, or more
+
+        bufferedIS = new BufferedInputStream(input);
+        bufferedIS.read(buffer,0,buffer.length);
+
+        System.out.println("Sending File:");
+        output.write(buffer,0,buffer.length);
+        output.flush();
+
+
     }
+
+}
